fb0759a93dfeb5b802724c29057f39557ae2dd8a
Erick G. Islas-Osuna <erickisos653@gmail.com>
2023-04-18 09:24:19 -0600

    Add pull and push to fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,4 @@
 vim.keymap.set('n', '<leader>gg', vim.cmd.Git, { desc = 'Open fu[g]itive' })
+vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { desc = 'Push to remote' })
+vim.keymap.set('n', '<leader>gF', ':Git pull<CR>', { desc = 'Pull from remote' })
 
